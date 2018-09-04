6e2a81f10bcb06916225e1a424dc49316c818362
Michael Arndt <antakor@outlook.de>
2018-09-04 22:28:28 +0200

    Added .dircolors to install script and repository

--- installzsh.sh
+++ installzsh.sh
@@ -11,6 +11,8 @@ SCRIPT=$(readlink -f $0)
 ln -v -b -s $(dirname "$SCRIPT")/Config/.zshrc ~/.zshrc
 ln -v -b -s $(dirname "$SCRIPT")/Config/antigen.zsh ~/antigen.zsh
 
+ln -v -b -s $(dirname "$SCRIPT")/Config/.dircolors ~/.dircolors
+
 ln -v -b -s $(dirname "$SCRIPT")/Config/.antigen/bundles/robbyrussell/oh-my-zsh/themes/agnoster-custom.zsh-theme ~/.antigen/bundles/robbyrussell/oh-my-zsh/themes/agnoster-custom.zsh-theme
 
 #git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k

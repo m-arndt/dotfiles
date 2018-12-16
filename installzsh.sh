8aef69b42690120db7a58aac0666ae22e540add7
Michael Arndt <antakor@outlook.de>
2018-12-16 00:52:15 +0000

    switched from antigen to zplug

--- installzsh.sh
+++ installzsh.sh
@@ -4,18 +4,18 @@ bash -c "./createessentiallinks.sh"
 
 sudo apt update
 
-sudo apt install zsh zsh-antigen fonts-font-awesome
+sudo apt install zsh fonts-font-awesome
 
 zsh -c 'exit'
 
 SCRIPT=$(readlink -f $0)
 
 ln -v -b -s $(dirname "$SCRIPT")/Config/.zshrc ~/.zshrc
-ln -v -b -s $(dirname "$SCRIPT")/Config/antigen.zsh ~/antigen.zsh
+#ln -v -b -s $(dirname "$SCRIPT")/Config/antigen.zsh ~/antigen.zsh
 
 ln -v -b -s $(dirname "$SCRIPT")/Config/.dircolors ~/.dircolors
 
-ln -v -b -s $(dirname "$SCRIPT")/Config/.antigen/bundles/robbyrussell/oh-my-zsh/themes/agnoster-custom.zsh-theme ~/.antigen/bundles/robbyrussell/oh-my-zsh/themes/agnoster-custom.zsh-theme
+#ln -v -b -s $(dirname "$SCRIPT")/Config/.antigen/bundles/robbyrussell/oh-my-zsh/themes/agnoster-custom.zsh-theme ~/.antigen/bundles/robbyrussell/oh-my-zsh/themes/agnoster-custom.zsh-theme
 
 #git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/powerline/fonts.git --depth=1

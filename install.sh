6df9fc1ca85d43718d4575508a5df1f58f011a3a
antakor <antakor@outlook.de>
2019-03-30 02:15:13 +0100

    changed tmux design and config completely i hope it works

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@
 sudo apt update
 sudo apt install tmux
 
-ln -v -b -s $(dirname $(realpath "$0"))/Config/.tmux.conf ~/.tmux.conf
+#ln -v -b -s $(dirname $(realpath "$0"))/Config/.tmux.conf ~/.tmux.conf
+ln -v -b -s $(dirname $(realpath "$0"))/.tmux ~/.tmux
+ln -v -b -s $(dirname $(realpath "$0"))/.tmux/.tmux.conf ~/.tmux.conf
+ln -v -b -s $(dirname $(realpath "$0"))/Config/.tmux.conf.local ~/.tmux.conf.local
 

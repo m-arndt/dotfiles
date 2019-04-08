486ca57acd06b126e87168dc979cead4f7922e9b
Michael Arndt <marn18@tu-clausthal.de>
2019-04-08 11:57:57 +0200

    changed .tmux no longer in Config but will be cloned directly

--- install_tmux.sh
+++ install_tmux.sh
@@ -6,11 +6,11 @@ sudo apt update
 sudo apt install tmux
 
 #ln -v -b -s $(dirname $(realpath "$0"))/Config/.tmux.conf ~/.tmux.conf
-ln -v -b -s -n $(dirname $(realpath "$0"))/.tmux ~/.tmux
-ln -v -b -s $(dirname $(realpath "$0"))/.tmux/.tmux.conf ~/.tmux.conf
+#ln -v -b -s -n $(dirname $(realpath "$0"))/.tmux ~/.tmux
+#ln -v -b -s $(dirname $(realpath "$0"))/.tmux/.tmux.conf ~/.tmux.conf
 ln -v -b -s $(dirname $(realpath "$0"))/Config/.tmux.conf.local ~/.tmux.conf.local
 
-(cd $(dirname $(realpath "$0")); git submodule update --init .tmux)
+(cd ~; git clone https://github.com/gpakosz/.tmux.git; ln -s -f .tmux/.tmux.conf);
 
-
+(cd ~; git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm);
 

efa0f1229db8e253cb60c2597f589fc0db4ebae8
Michael Arndt <antakor@outlook.de>
2019-04-10 18:34:58 +0200

    fixed xfce4terminal configscript

--- install-scripts/xfce4-terminal-config.sh
+++ install-scripts/xfce4-terminal-config.sh
@@ -3,5 +3,6 @@
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 DOTFILES_ROOT=$(dirname "$(dirname $(realpath "$0"))")
 
+mkdir -p ~/.local/share/xfce4/terminal/colorschemes
 ln -v -b -s $DOTFILES_ROOT/Config/.local/share/xfce4/terminal/colorschemes/monokai-dark.theme ~/.local/share/xfce4/terminal/colorschemes/monokai-dark.theme
 

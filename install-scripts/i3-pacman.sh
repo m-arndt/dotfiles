0ee5fc8535e6a65434cf8c24746c5f82b35c4235
Michael Arndt <marn18@tu-clausthal.de>
2019-04-10 10:18:45 +0200

    fixed root

--- install-scripts/i3-pacman.sh
+++ install-scripts/i3-pacman.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 DOTFILES_ROOT=$(dirname "$(dirname $(realpath "$0"))")
 
-pacman -S i3 i3blocks rofi feh
+sudo pacman -S i3 i3blocks rofi feh
 
 $SCRIPT_DIR/i3-config.sh

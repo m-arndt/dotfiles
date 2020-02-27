4b7a39da79e7370ae136eb3b42a92c1e98541ef1
Michael Arndt <antakor@outlook.de>
2020-02-27 10:51:45 +0100

    updated rust install curl command

--- install-scripts/rust.sh
+++ install-scripts/rust.sh
@@ -4,4 +4,4 @@ set -x
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DOTFILES_ROOT=$(dirname "$SCRIP_DIR")
 
-curl https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh

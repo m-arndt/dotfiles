f3c99fe84180f7a45090fe40caab6ee8accd3366
Michael Arndt <antakor@outlook.de>
2020-03-23 16:50:23 +0100

    better vscode.ps1

--- install-scripts/vscode.ps1
+++ install-scripts/vscode.ps1
@@ -5,8 +5,8 @@ Write-Host "Current script directory is $DotfilesRoot"
 
 $UserSettings = "$env:USERPROFILE\AppData\Roaming\Code\User\settings.json"
 
-Remove-Item $env:USERPROFILE\AppData\Roaming\Code\User\settings.json -ErrorAction Ignore
-New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\AppData\Roaming\Code\User\settings.json -Target $DotfilesRoot\config-files\all\home\username\.config\Code\User\settings.json
+Remove-Item $UserSettings -ErrorAction Ignore
+New-Item -ItemType SymbolicLink -Path $UserSettings -Target $DotfilesRoot\config-files\all\home\username\.config\Code\User\settings.json
 
 foreach ($line in Get-Content $ScriptDir\resources\vscode_extensions){
     code --install-extension $line
